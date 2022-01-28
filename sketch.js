52b35930073f0959fff0a790bf9b57ceaad63fac
KritiKapoor2010 <98330511+KritiKapoor2010@users.noreply.github.com>
2022-01-28 18:51:34 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,13 +2,10 @@ var sea,ship;
 var seaImg,shipImg;
 
 function preload(){
-  //uncomment the code to add animation to ship 
+  
 
-  shipImg1 = loadAnimation("ship-1.png");
-  //shipImg1 = loadAnimation("ship-1.png");
-  //shipImg1 = loadAnimation("ship-1");
-  //shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
-  //shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
+  shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+  
   
   seaImg = loadImage("sea.png");
 }
@@ -34,12 +31,12 @@ function draw() {
   background(0);
   sea.velocityX = -3;
 
-  //uncomment code to reset the background
+  
   if(sea.x < 0){
-    //sea.x = 0;
-    //sea.x = sea.width;
-    //sea.x = sea.width/8;
-    //sea.y = height;
+    sea.x = 0;
+    sea.x = sea.width;
+    sea.x = sea.width/8;
+    sea.y = height;
   }
 
  
